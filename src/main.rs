f8e64bbd5ede0ca1cf73987a3d0e9957dd740707
Geoffrey Brunet <geoffrey.brunet@icloud.com>
2021-04-17 14:46:16 +0200

    reformat code and change pgconnection to pgpool

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 //! src/main.rs
 use std::net::TcpListener;
-use zero2prod::run;
+use sqlx::PgPool;
 use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration");
-    // We have removed the hard-coded`8000`- it's now coming from our settings!
-    let address =format!("127.0.0.1:{}", configuration.application_port);
-    let address = TcpListener::bind(address)?;
-    run(address)?.await
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
 }
